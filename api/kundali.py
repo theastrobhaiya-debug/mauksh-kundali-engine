583f1f306db04f3ddb41d06c09e756eedbdec3bf
theastrobhaiya-debug <theastrobhaiya@gmail.com>
2026-08-27 20:00:10 +0530

    Update kundali.py

--- api/kundali.py
+++ api/kundali.py
@@ -1,8 +1,10 @@
 import swisseph as swe
 from datetime import datetime, timezone
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 from api.location import location_api
 
 app.register_blueprint(
