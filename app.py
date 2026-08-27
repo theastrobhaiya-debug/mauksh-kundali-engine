ea77672fe2d8baed010db62db5ed290ed193cd06
theastrobhaiya-debug <theastrobhaiya@gmail.com>
2026-08-27 23:39:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from api.panchang import panchang_api
+from api.transits import transits_api
 
 app = Flask(__name__)
 
 # Register Panchang API
 app.register_blueprint(panchang_api)
+app.register_blueprint(transits_api)
 
 
 @app.route("/")
