3bb868c08243b9f246043867e8d5226ec7371e4a
theastrobhaiya-debug <theastrobhaiya@gmail.com>
2026-08-27 20:06:50 +0530

    Implement custom CORS headers in kundali.py
    Replaced CORS initialization with a custom after_request function to add CORS headers.

--- api/kundali.py
+++ api/kundali.py
@@ -1,10 +1,18 @@
 import swisseph as swe
 from datetime import datetime, timezone
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+@app.after_request
+def add_cors_headers(response):
+
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+
+    return response
+
+
 from api.location import location_api
 
 app.register_blueprint(
